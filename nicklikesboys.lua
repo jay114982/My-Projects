1958197f297e01bfa593f3385d6345703c6415b5
jay114982 <jay114982@gmail.com>
2026-08-03 22:47:11 -0400

    Add files via upload

---
+++ nicklikesboys.lua
@@ -0,0 +1,51 @@
+local G2L = {};
+
+-- StarterGui.ScreenGui
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.ScreenGui.Frame
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(45, 45, 45);
+G2L["2"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+G2L["2"]["Size"] = UDim2.new(0, 100, 0, 100);
+G2L["2"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"].Draggable = true
+G2L["2"].Selectable = true
+G2L["2"].Active = true
+G2L["2"].ResetOnSpawn = false
+
+
+-- StarterGui.ScreenGui.Frame.UICorner
+G2L["3"] = Instance.new("UICorner", G2L["2"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.TextLabel
+G2L["4"] = Instance.new("TextLabel", G2L["2"]);
+G2L["4"]["TextWrapped"] = true;
+G2L["4"]["BorderSizePixel"] = 0;
+G2L["4"]["TextSize"] = 14;
+G2L["4"]["TextScaled"] = true;
+G2L["4"]["BackgroundColor3"] = Color3.fromRGB(55, 55, 55);
+G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Heavy, Enum.FontStyle.Normal);
+G2L["4"]["TextColor3"] = Color3.fromRGB(91, 91, 91);
+G2L["4"]["Size"] = UDim2.new(1, 0, 0.8, 0);
+G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["Text"] = [[Bank]];
+G2L["4"]["Position"] = UDim2.new(0, 0, 0.2, 0);
+G2L["4"].MouseButton1Click:Connect(function()
+	game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1118.9351806640625, 8.221870422363281, -329.87969970703125)
+end
+
+
+-- StarterGui.ScreenGui.Frame.TextLabel.UICorner
+G2L["5"] = Instance.new("UICorner", G2L["4"]);
+
+
+
+
+return G2L["1"], require;
